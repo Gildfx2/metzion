3db610335e99e23ecc8fec384ff8bcc6170db94f
gil <gil3dfx@gmail.com>
2023-11-01 08:57:52 +0200

    after navigation bar

--- app/src/main/java/com/example/pre_alpha/MainActivity.java
+++ app/src/main/java/com/example/pre_alpha/MainActivity.java
@@ -1,29 +1,62 @@
 package com.example.pre_alpha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     FirebaseAuth auth;
-
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment=new HomeFragment();
+    SearchFragment searchFragment=new SearchFragment();
+    PostFragment postFragment=new PostFragment();
+    FavouriteFragment favouriteFragment=new FavouriteFragment();
+    UserFragment userFragment=new UserFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         auth=FirebaseAuth.getInstance();
-        tv=findViewById(R.id.idTry);
-        FirebaseUser user=auth.getCurrentUser();
-        tv.setText(user.getEmail());
+        bottomNavigationView=findViewById(R.id.bottomNavigationBar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, searchFragment).commit();
+                        return true;
+                    case R.id.post:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, postFragment).commit();
+                        return true;
+                    case R.id.favourite:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, favouriteFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, userFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
 
     }
 
@@ -32,7 +65,9 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = remember.edit();
         editor.putString("remember", "false");
         editor.commit();
+        auth.signOut();
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+        finish();
     }
 }
