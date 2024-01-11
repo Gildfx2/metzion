e30e8e7be92a7ec4ceed5aa457c5539374191234
Gildfx2 <gil3dfx@gmail.com>
2024-01-11 09:39:32 +0200

    progress of Notification

---
+++ app/src/main/java/com/example/pre_alpha/notifications/APIService.java
@@ -0,0 +1,8 @@
+package com.example.pre_alpha.notifications;
+
+import retrofit2.http.Headers;
+
+public interface APIService {
+
+
+}
--- app/src/main/java/com/example/pre_alpha/notifications/MessageNotification.java
+++ app/src/main/java/com/example/pre_alpha/notifications/MessageNotification.java
@@ -3,8 +3,10 @@ package com.example.pre_alpha.notifications;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.net.Uri;
 import android.os.Build;
 
 public class MessageNotification extends ContextWrapper {
@@ -24,6 +26,28 @@ public class MessageNotification extends ContextWrapper {
         notificationChannel.enableLights(true);
         notificationChannel.enableVibration(true);
         notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        getManeger().createNotificationChannel(notificationChannel);
+    }
+
+    public NotificationManager getManeger(){
+        if(notificationManager == null){
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return notificationManager;
+    }
+
+    public Notification.Builder getNotifications(String title,
+                                                 String body,
+                                                 PendingIntent pIntent,
+                                                 Uri soundUri,
+                                                 String icon){
+        return new Notification.Builder(getApplicationContext(), ID)
+                .setContentIntent(pIntent)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSound(soundUri)
+                .setAutoCancel(true)
+                .setSmallIcon(Integer.parseInt(icon));
     }
 
 }
